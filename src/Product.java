95ad72fdeef2576ef86ef77c53195e4a6e19a1a5
Fatcat0712 <maisonhai456@gmail.com>
2026-06-02 10:01:56 +0700

    publish release vv1.1.0

--- src/Product.java
+++ src/Product.java
@@ -2,4 +2,8 @@ public class Product {
     public void printProduct() {
         System.out.println("Product entity was added to the project");
     }
+
+    public void addProduct() {
+        System.out.println("Product was added to the database");
+    }
 }
