8e98c3551bc5bcd751fa0ee838f9175e735d2250
Fatcat0712 <maisonhai456@gmail.com>
2026-06-02 09:56:50 +0700

    #4 - create Product entity java file

--- src/Product.java
+++ src/Product.java
@@ -1,5 +1,5 @@
 public class Product {
     public void printProduct() {
-        System.out.println("Product entity was added");
+        System.out.println("Product entity was added to the project");
     }
 }
